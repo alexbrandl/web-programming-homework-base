3f8e87131f1b5f576c7098b83165ef9d0ff139a6
alexbrandl <abrandl#umass.edu>
2021-04-12 20:45:53 -0400

    done woohoo

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(express.urlencoded({extended: true}))
-  .get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-  })
-  .get('/configuremenu', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/configuremenu.html'));
-  })
-  .get('/api/entrees', function(req, res) {
+.use(express.static(path.join(__dirname, 'public')))
+.use(express.urlencoded({extended: true}))
+.get('/', function(req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+})
+.get('/configuremenu', function(req, res) {
+res.sendFile(path.join(__dirname + '/public/configuremenu.html'));
+})
+.get('/api/entrees', function(req, res) {
     res.json(entreeList); 
-  })
-  .delete('/api/entrees/:id', function(req, res) {
-    let arr = []
-    for (var i = 0; i < entreeList.length; i++) {
-      if (req.params.id != entreeList[i].id) {
-        arr.push(entreeList[i]);
-      }
-    }
-    entreeList = arr;
+})
+.delete('/api/entrees/:id', function(req, res) {
+  arr = entreeList.filter(function(elem){
+  return elem.id!=req.params.id;
+});
+entreeList = arr;
     res.sendStatus(204);
   })
 .post('/api/entrees/', function(req, res) {
-    console.log(req.body)
-  })
+    entreeList.push(req.body);
+    res.sendStatus(204);
+  })
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   var entreeList = [{ id: "chickenfriedsteak", name: "Chicken Fried Steak"},
   { id: "ribeyesteak", name: "Ribeye Steak"},
